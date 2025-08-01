460b466c0343f478d3e2100e2ea79ad0785e1983
KalaivaniS1994 <kalaivanis199406@gmail.com>
2025-08-01 11:09:06 +0530

    Add files via upload

---
+++ task2_script.sh
@@ -0,0 +1,32 @@
+kiran@LAPTOP-F1ML98NA:~/dir1$ touch lm1.txt
+kiran@LAPTOP-F1ML98NA:~/dir1$ touch lm.txt
+kiran@LAPTOP-F1ML98NA:~/dir1$ touch lm2.txt
+kiran@LAPTOP-F1ML98NA:~/dir1$ touch lm3.txt
+kiran@LAPTOP-F1ML98NA:~/dir1$ touch lm4.txt
+kiran@LAPTOP-F1ML98NA:~/dir1$ touch lm5.txt
+kiran@LAPTOP-F1ML98NA:~/dir1$ touch lm6.txt
+kiran@LAPTOP-F1ML98NA:~/dir1$ touch lm7.txt
+kiran@LAPTOP-F1ML98NA:~/dir1$ touch lm8.txt
+kiran@LAPTOP-F1ML98NA:~/dir1$ touch lm9.txt
+kiran@LAPTOP-F1ML98NA:~/dir1$ touch lm10.txt
+touch lm11.txt
+touch lm12.txt
+touch lm13.txt
+touch lm14.txt
+touch lm15.txt
+touch lm16.txt
+touch lm17.txt
+touch lm18.txt
+touch lm19.txt
+mv lm1.txt lm1.yml
+mv lm2.txt lm2.yml
+mv lm3.txt lm3.yml
+mv lm5.txt lm5.yml
+mv lm6.txt lm6.yml
+ls
+ls -lt | head -n 5
+total 0
+-rw-r--r-- 1 kiran kiran 0 Aug  1 05:05 lm19.txt
+-rw-r--r-- 1 kiran kiran 0 Aug  1 05:04 lm18.txt
+-rw-r--r-- 1 kiran kiran 0 Aug  1 05:04 lm17.txt
+-rw-r--r-- 1 kiran kiran 0 Aug  1 05:04 lm16.txt
