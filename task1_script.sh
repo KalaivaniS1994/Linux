bfe884fae86392b8246b7315c7f8a885ea1e75f8
KalaivaniS1994 <kalaivanis199406@gmail.com>
2025-08-01 09:34:51 +0530

    Add files via upload

---
+++ task1_script.sh
@@ -0,0 +1,6 @@
+mkdir my_folder && cd my_folder
+echo "CMA CGM is a global leader in Shpping and logistcs" > myfile.txt
+echo "They operate vast network and serving over 420 ports" > another_file.txt
+cat another_file.txt >> myfile.txt
+cat myfile.txt
+ls
